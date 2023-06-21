033e43f5584402443ea7c8daf7246eb6a3f82269
Ivan <techydad05@gmail.com>
2023-06-21 18:49:25 -0400

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,7 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,http://192.168.1.42:9002,http://localhost,http://192.168.1.42:9002/app";
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,http://192.168.1.42,localhost";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000,http://localhost,http://192.168.1.42,http://192.168.1.42:9002";
