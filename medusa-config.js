5e7dd08e56f2eb562aeecfbb40bc62315fdafd0a
Ivan <techydad05@gmail.com>
2023-06-21 18:28:15 -0400

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -26,7 +26,7 @@ const ADMIN_CORS =
   process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001,http://192.168.1.42:9002,http://localhost,http://192.168.1.42:9002/app";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000,http://localhost,http://192.168.1.42";
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000,http://localhost,http://192.168.1.42,http://192.168.1.42:9002";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
